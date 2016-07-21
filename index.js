cdc96b73a66c14aee2415fdac394c5ae22181299
Robert Dickert <robert.dickert@gmail.com>
2016-07-21 17:50:38 -0600

    fixed: newCollection() didn'd work with td.verify(). Also added methods to Mongo and Cursor
    Can now test td.verify(MyCollection.find('123').fetch());

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const td = require('testdouble');
 
 // Meteor core
@@ -11,17 +13,26 @@ Object.assign(Meteor, {
 
 // Collections
 
-const newCollection = () => ({
-  find: () => ({ fetch: td.function('fetch') }),
-  findOne: () => td.function('findOne'),
-  insert: () => td.function('insert'),
-  remove: () => td.function('remove'),
-});
+// use newCollection() to stub a Meteor collection that you would have created with
+// `new Meteor.Collection('somename')`. You must call it with parens () - this allows
+// you to have more than one. The default behavior of find() is that for any
+// arguments passed, it will return a stubbed cursor object that you can
+// test with td.verify(). If you need more specific behavior, you should create a
+// custom stub rather than using this one.
+
+const createCursor = () => td.object(['forEach', 'map', 'fetch', 'count', 'observe', 'observeChanges']);
+
+const newCollection = () => {
+  const collection = td.object(['find', 'findOne', 'insert', 'remove', 'upsert', 'allow', 'deny']);
+  td.when(collection.find(td.matchers.anything())).thenReturn(createCursor());
+  return collection;
+};
 
 const Mongo = td.object(['Collection', 'Cursor', 'ObjectID']);
 
 module.exports = {
   Meteor,
+  createCursor,
   newCollection,
   Mongo,
 };
